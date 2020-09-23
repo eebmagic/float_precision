29c7c67833aa5ba27e332010768be34427a2dcf4
Ethan B <eebmagic@gmail.com>
2020-09-23 00:41:11 -0400

    added cpp version

---
+++ cpp/script.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout << "test" << endl;
+    // cout << bitset<32>(123) << " - " << 123 << endl;
+    float end = 4294967296;
+    for (float i = 0; i < end; i++) {
+        // cout << bitset<32>(i) << " - " << i << " - " << i / end << endl;
+        cout << bitset<32>(i) << " - " << i << endl;
+    }
+}
